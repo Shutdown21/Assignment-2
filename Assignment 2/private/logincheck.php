feb0df5eedee11e2e3475106486f415346106c8c
Gabriel Hubert <hube0078@algonquinlive.com>
2023-11-23 16:21:41 -0500

    hi

--- Assignment 2/private/logincheck.php
+++ Assignment 2/private/logincheck.php
@@ -7,30 +7,39 @@ require_once('../private/database.php');
 $db = db_connect();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["Username"];
-    $password = $_POST["Password"];
+    $username = $_POST["username"];
+    $password = $_POST["password"];
 
-    $sql = "SELECT id, Username, Password FROM account WHERE Username = '$username'";
-    
+    $sql = "SELECT Username, Password FROM account WHERE Username = '$username'";
+
     $result_set = mysqli_query($db, $sql);
     
     if ($result_set) {
         $user = mysqli_fetch_assoc($result_set);
-
-        if ($user && password_verify($password, $user['Password'])) {
-            // Successful login
+        if ($user && $user[$username]===$password) {
+                // Successful login
             $_SESSION['user_id'] = $user['id'];
-            header("Location: ../public/account.php"); // Redirect to the account page
-            exit();
-        } else {
-            // Invalid username or password
-            header("Location: ../public/login.php?login_error=true");
-            exit();
+            header("Location: ../public/account.php");
+            
+        // if(isset($user[$username])&& $user[$username]===$password){
+        //     $_SESSION['user_id'] = $username;
+        //     $_SESSION['valid_pass'] = $password;
+            
         }
-    } else {
-        // Database query error
-        echo "Database query error: " . mysqli_error($db);
+        else {
+            header("Location: ../public/data-display.php");
+        }
     }
+
+    //     } else {
+    //         // Invalid username or password
+    //         header("Location: ../public/login.php?login_error=true");
+    //         exit();
+    //     }
+    // } else {
+    //     // Database query error
+    //     echo "Database query error: " . mysqli_error($db);
+    // }
 }
 
 // Close the database connection
