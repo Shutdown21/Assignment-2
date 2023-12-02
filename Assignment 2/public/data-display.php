9229ab548cde084a6813269aadc98a4d008fafa0
Exscid <emilepapineau2003@gmail.com>
2023-12-01 21:34:32 -0500

    filters added and css changes

--- Assignment 2/public/data-display.php
+++ Assignment 2/public/data-display.php
@@ -17,32 +17,52 @@ if (isset($_SESSION['user_id'])) {
 } ?>
 
 <section id="data-display">
-    <h2>Data Display with Search</h2>
-
     <form id="search-form" method="post">
-        <label for="search">Search:</label>
+        <h2>Search</h2>
         <input type="text" id="search" name="search">
-        
-
         <button type="submit">Search</button>
     </form>
 
+    <form id="filter-form" method="post">
+        <h2>Filter</h2>
+        <label for="developer">Developer:</label>
+        <input type="text" id="developer" name="developer">
+
+        <label for="platform">Platform:</label>
+        <input type="text" id="platform" name="platform">
+
+        <label for="genre">Genre:</label>
+        <input type="text" id="genre" name="genre">
+
+        <button type="submit">Apply Filters</button>
+    </form>
+
     <div id="display-container">
-        <?php
-            require_once('../private/database.php');
-            $db = db_connect();
+    <?php
+                    require_once('../private/database.php');
+                    $db = db_connect();
 
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                $searchTerm = mysqli_real_escape_string($db, $_POST['search']);
+                    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['search'])) {
+                        $searchTerm = mysqli_real_escape_string($db, $_POST['search']);
+                        $sql = "SELECT * FROM games WHERE 
+                                Name LIKE '%$searchTerm%' OR
+                                Developer LIKE '%$searchTerm%' OR
+                                Platform LIKE '%$searchTerm%' OR
+                                Genre LIKE '%$searchTerm%'";
+                    }
+                    elseif ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['developer'])) {
+                        $developer = mysqli_real_escape_string($db, $_POST['developer']);
+                        $platform = mysqli_real_escape_string($db, $_POST['platform']);
+                        $genre = mysqli_real_escape_string($db, $_POST['genre']);
 
-                $sql = "SELECT * FROM games WHERE 
-                        Name LIKE '%$searchTerm%' OR
-                        Developer LIKE '%$searchTerm%' OR
-                        Platform LIKE '%$searchTerm%' OR
-                        Genre LIKE '%$searchTerm%'";
-            } else {
-                $sql = "SELECT * FROM games";
-            }
+                        $sql = "SELECT * FROM games WHERE 
+                                (Developer LIKE '%$developer%' OR '$developer' = '') AND
+                                (Platform LIKE '%$platform%' OR '$platform' = '') AND
+                                (Genre LIKE '%$genre%' OR '$genre' = '')";
+                    }
+                    else {
+                        $sql = "SELECT * FROM games";
+                    }
 
             $result_set = mysqli_query($db, $sql);
 
